a2575b5701a65d1caf82d92a045ab1ea3557e5e7
nakhilroynr244-hub <nakhilroy.nr244@gmail.com>
2026-07-17 22:29:52 +0530

    Added initiate_payment.php

--- initiate_payment.php
+++ initiate_payment.php
@@ -18,3 +18,32 @@ $paytmParams["body"] = array(
         "currency"  => "INR",
     ),
     "userInfo"      => array(
+        "custId"    => "CUST_001",
+    ),
+);
+
+/*
+* Generate checksum by parameters we have in body
+* Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytmpayments.com/next/apikeys 
+*/
+$checksum = PaytmChecksum::generateSignature(json_encode($paytmParams["body"], JSON_UNESCAPED_SLASHES), "YOUR_MERCHANT_KEY");
+
+$paytmParams["head"] = array(
+    "signature"    => $checksum
+);
+
+$post_data = json_encode($paytmParams, JSON_UNESCAPED_SLASHES);
+
+/* for Staging */
+$url = "https://securestage.paytmpayments.com/theia/api/v1/initiateTransaction?mid=YOUR_MID_HERE&orderId=ORDERID_98765";
+
+/* for Production */
+// $url = "https://secure.paytmpayments.com/theia/api/v1/initiateTransaction?mid=YOUR_MID_HERE&orderId=ORDERID_98765";
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json")); 
+$response = curl_exec($ch);
+print_r($response);
